a15d88de681d46dcd7a96facd20585f3eb2604c4
Ganesh Maharnur <ganeshmaharnur11@gmail.com>
2023-07-31 23:41:53 +0530

    remove some unusable code

--- src/components/ShowDetails.js
+++ src/components/ShowDetails.js
@@ -1,37 +1,37 @@
-import CloudIcon from '@mui/icons-material/Cloud';
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
-import OpacityIcon from '@mui/icons-material/Opacity';
-import SevereColdIcon from '@mui/icons-material/SevereCold';
-import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
-import React, { useEffect, useState } from 'react';
-import '../css/Details.css'
+import CloudIcon from "@mui/icons-material/Cloud";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import OpacityIcon from "@mui/icons-material/Opacity";
+import SevereColdIcon from "@mui/icons-material/SevereCold";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import React, { useEffect, useState } from "react";
+import "../css/Details.css";
 
 const ShowDetails = ({ data }) => {
-  const [icon,setIcon] = useState(<WbSunnyIcon />);
+  const [icon, setIcon] = useState(<WbSunnyIcon />);
 
-  const weatherIcon = data.data.weather[0].icon
+  const weatherIcon = data.data.weather[0].icon;
   useEffect(() => {
     switch (weatherIcon) {
-      case '01d':
+      case "01d":
         setIcon(<WbSunnyIcon />);
         break;
-      case '02d':
-      case '03d':
-      case '04d':
+      case "02d":
+      case "03d":
+      case "04d":
         setIcon(<CloudIcon />);
         break;
-      case '09d':
-      case '10d':
+      case "09d":
+      case "10d":
         setIcon(<OpacityIcon />);
         break;
-      case '11d':
+      case "11d":
         setIcon(<ThunderstormIcon />);
         break;
-      case '13d':
+      case "13d":
         setIcon(<SevereColdIcon />);
         break;
-      case '50d':
+      case "50d":
         setIcon(<FormatAlignCenterIcon />);
         break;
       default:
@@ -40,25 +40,33 @@ const ShowDetails = ({ data }) => {
   }, [weatherIcon]);
 
   return (
-    <div className='details'>
-      <h2 style={{ marginBottom: 0}}>{data.data.name}</h2>
-      <h2 style={{ marginBottom: 0}}>{data.data.main.temp}°C</h2>
-      <div className='min_max_temp'>
-        Min {data.data.main.temp_min}°C |
-        Max {data.data.main.temp_min}°C 
+    <div className="details">
+      <h2 style={{ marginBottom: 0 }}>{data.data.name}</h2>
+      <h2 style={{ marginBottom: 0 }}>{data.data.main.temp}°C</h2>
+      <div className="min_max_temp">
+        Min {data.data.main.temp_min}°C | Max {data.data.main.temp_min}°C
       </div>
-      <div className='weather_description'>
-        <h4 style={{ marginRight: '10px'}}><span className='description_span'>Description: </span>{data.data.weather[0].description}</h4>
-        { icon }
+      <div className="weather_description">
+        <h4 style={{ marginRight: "10px" }}>
+          <span className="description_span">Description: </span>
+          {data.data.weather[0].description}
+        </h4>
+        {icon}
       </div>
-      <div className='wind_speed'>
-        <label style={{ marginRight: '10px'}}><span className='description_span'>Wind Speed : </span>{data.data.wind.speed}</label>
+      <div className="wind_speed">
+        <label style={{ marginRight: "10px" }}>
+          <span className="description_span">Wind Speed : </span>
+          {data.data.wind.speed}
+        </label>
       </div>
-      <div className='humidity'>
-        <label style={{ marginRight: '10px'}}><span className='description_span'>Humidity : </span>{data.data.main.humidity}</label>
+      <div className="humidity">
+        <label style={{ marginRight: "10px" }}>
+          <span className="description_span">Humidity : </span>
+          {data.data.main.humidity}
+        </label>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShowDetails
+export default ShowDetails;
--- src/components/WeatherPage.js
+++ src/components/WeatherPage.js
@@ -4,7 +4,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import useToastMessage from "../customHook/useToastMessage";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 import ShowDetails from "./ShowDetails";
 import axios from "axios";
 import weatherReducer from "../reducer/WeatherReducer";
@@ -15,7 +15,7 @@ const WeatherPage = () => {
     showWeatherDetails: false,
     showLoader: false,
     weatherData: {},
-  }
+  };
 
   const [state, dispatch] = useReducer(weatherReducer, initialState);
   const { cityName, showWeatherDetails, showLoader, weatherData } = state;
@@ -23,27 +23,28 @@ const WeatherPage = () => {
   const { showToast, ToastContainer } = useToastMessage();
 
   const getWeatherData = (e) => {
-    dispatch({ type: 'SET_SHOW_WEATHER_DETAILS', payload: false });
+    dispatch({ type: "SET_SHOW_WEATHER_DETAILS", payload: false });
     e.preventDefault();
-    if(cityName === ''){
-        showToast("City name is mandatory.", "warning");
+    if (cityName === "") {
+      showToast("City name is mandatory.", "warning");
     } else {
-        dispatch({ type: 'SET_SHOW_LOADER', payload: true });
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7b9a283b3d059435e1eb181876f7fd96`)
-        .then(response => {
-          // debugger;
-          console.log(response);
-          dispatch({ type: 'SET_SHOW_LOADER', payload: false });
-          dispatch({ type: 'SET_WEATHER_DATA', payload: response });
-          dispatch({ type: 'SET_SHOW_WEATHER_DETAILS', payload: true });
-          dispatch({ type: 'SET_CITY_NAME', payload: '' });
+      dispatch({ type: "SET_SHOW_LOADER", payload: true });
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7b9a283b3d059435e1eb181876f7fd96`
+        )
+        .then((response) => {
+          dispatch({ type: "SET_SHOW_LOADER", payload: false });
+          dispatch({ type: "SET_WEATHER_DATA", payload: response });
+          dispatch({ type: "SET_SHOW_WEATHER_DETAILS", payload: true });
+          dispatch({ type: "SET_CITY_NAME", payload: "" });
         })
-        .catch(error => {
-          dispatch({ type: 'SET_SHOW_LOADER', payload: false });
-          if(error.response.data.cod === "404"){
-              showToast(error.response.data.message, "warning");
+        .catch((error) => {
+          dispatch({ type: "SET_SHOW_LOADER", payload: false });
+          if (error.response.data.cod === "404") {
+            showToast(error.response.data.message, "warning");
           }
-          dispatch({ type: 'SET_SHOW_WEATHER_DETAILS', payload: false });
+          dispatch({ type: "SET_SHOW_WEATHER_DETAILS", payload: false });
         });
     }
   };
@@ -59,15 +60,17 @@ const WeatherPage = () => {
               label="Enter city"
               variant="outlined"
               value={cityName}
-              onChange={(e) => dispatch({ type: 'SET_CITY_NAME', payload: e.target.value })}
+              onChange={(e) =>
+                dispatch({ type: "SET_CITY_NAME", payload: e.target.value })
+              }
             />
             <Button type="submit" variant="contained">
               check weather
             </Button>
           </Stack>
         </form>
-        { showLoader && <CircularProgress /> }
-        { showWeatherDetails && <ShowDetails data={weatherData} /> }
+        {showLoader && <CircularProgress />}
+        {showWeatherDetails && <ShowDetails data={weatherData} />}
       </div>
     </>
   );
